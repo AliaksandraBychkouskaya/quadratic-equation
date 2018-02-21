cf12233224c71b32b8471d8638d425f47a8b8144
AliaksandraBychkouskaya <aleksandra.bychkovskaya95@gmail.com>
2018-02-22 00:34:55 +0300

    solution for quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function solveEquation(equation) {
+function solveEquation(equation) {
   // your implementation
-}
+  let a, b, c, x1, x2, strB, strC;
+  let arrEquation= [];
+  let arr = [];
+  let arrSolutions = [];
+  let equationModified;
+  equationModified = equation.substring(0, equation.length);
+  arr = equationModified.split(' ');
+  a = + arr[0];
+
+  strB = arr[3]+arr[4];
+  b = + strB;
+
+  strC = arr[7]+arr[8];
+  c = + strC;
+
+  x1 = Math.round((-b + Math.sqrt(Math.pow(b,2) - 4 * a * c))/ (2 * a));
+  x2 = Math.round((-b - Math.sqrt(Math.pow(b,2) - 4 * a * c))/ (2 * a));
+
+  arrSolutions.push(x1);
+  arrSolutions.push(x2);
+  arrSolutions.sort((a,b) => (a - b));
+  return arrSolutions;
+};
+
+
+
+module.exports = solveEquation;
